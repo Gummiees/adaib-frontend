19f8c22e01b2d8375f43d5901e26a294ff357287
Gummiees <xavicaromiro@gmail.com>
2025-09-05 18:09:04 +0200

    refactor(footer): remove footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from '@features/footer/footer.component';
 import { NavbarComponent } from '@features/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
 })
 export class App {}
