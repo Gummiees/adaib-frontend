a82f7f4379b3141ae6b97dc7e7149a103fed0d97
Gummiees <xavicaromiro@gmail.com>
2025-09-08 19:58:38 +0200

    Untrack environment files from git

--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-import { provideStoreDevtools } from '@ngrx/store-devtools';
-
-export const environment = {
-  apiUrl: 'http://localhost:5194/api',
-  isProduction: false,
-  providers: [provideStoreDevtools({ maxAge: 25 })],
-};
--- src/environments/environment.production.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  apiUrl: 'https://basketwebapi-production.up.railway.app/api',
-  isProduction: true,
-  providers: [],
-};
